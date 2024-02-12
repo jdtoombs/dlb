953563d662f57a5269db9adea4ad855d1e5ff552
jdtoombs <jdtoombs@gmail.com>
2024-02-11 21:37:10 -0800

    debug pt.2

--- api/controllers/openai.controller.js
+++ api/controllers/openai.controller.js
@@ -2,9 +2,18 @@ import openaiService from "../services/openai/openai.service.js";
 
 const controller = {};
 
+/**
+ * Asynchronously transcribes an audio file uploaded by the user.
+ * @function transcribe
+ * @async
+ * @param {object} req - The request object from Express.js, expected to contain a file in `req.file`.
+ * @param {object} res - The response object from Express.js, used to send back the transcribed text.
+ * @returns {Promise<void>} Sends a JSON response with the transcription. Does not return a value.
+ */
 controller.transcribe = async (req, res) => {
   const filePath = req.file.path;
-  const transcript = await openaiService.transcribeAudio(filePath);
+  const transcript = openaiService.transcribeAudio(filePath);
+  console.log(transcript);
   res.json({ transcript });
 };
 
--- api/services/openai/openai.service.js
+++ api/services/openai/openai.service.js
@@ -5,9 +5,17 @@ import fs from "fs";
 const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
 const openaiService = {};
 
+/**
+ * Transcribe an audio file using OpenAI's API
+ * @function transcribeAudio
+ * @async
+ * @param {string} audio - The path to the audio file
+ * @returns {string} - The transcribed text
+ */
 openaiService.transcribeAudio = async (audio) => {
   console.log("Transcribing audio...", audio);
   try {
+    // timing out here
     const transcription = await openai.audio.transcriptions.create({
       file: fs.createReadStream(audio),
       model: "whisper-1",
@@ -15,9 +23,10 @@ openaiService.transcribeAudio = async (audio) => {
     return transcription.text;
   } catch (err) {
     if (err instanceof OpenAI.APIError) {
-      console.log(err.status); // 400
-      console.log(err.name); // BadRequestError
-      console.log(err.headers); // {server: 'nginx', ...}
+      console.log("API Error", err);
+      console.log(err.status);
+      console.log(err.name);
+      console.log(err.headers);
     } else {
       throw err;
     }
